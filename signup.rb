fa7f568cd1ad9691aa3eb0412f4f1da0e9b91e59
tnx6devine <tnx6devine@gmail.com>
2017-11-29 18:37:31 -0500

    set up username validator

--- signup.rb
+++ signup.rb
@@ -1,4 +1,9 @@
 def set_up_username(username)
 	username = username.to_s
-	username.class
+	# username.class -- that was to test to make sure usernames were strings only
+	if username.length > 5
+		"valid"
+	else
+		"invalid"
+	end
 end	
--- signup_test.rb
+++ signup_test.rb
@@ -5,20 +5,20 @@ class TestSignup < Minitest::Test
 	def test_assert_1_is_1
 		assert_equal(1, 1)
 	end
+# following tests were to determine username datatype.  no longer needed
+	# def test_assert_that_usernames_are_strings
+	# 	username = "trixie"
+	# 	assert_equal(String, set_up_username(username))
+	# end
 
-	def test_assert_that_usernames_are_strings
-		username = "trixie"
-		assert_equal(String, set_up_username(username))
-	end
-
-	def test_assert_that_integers_are_still_strings
-		username = 1254
-		assert_equal(String, set_up_username(username))
-	end
+	# def test_assert_that_integers_are_still_strings
+	# 	username = 1254
+	# 	assert_equal(String, set_up_username(username))
+	# end
 
 	def test_assert_that_5_char_username_is_invalid
 		username = 79687
-		assert_equal(false, set_up_username(username))
+		assert_equal("invalid", set_up_username(username))
 	end
 
 end
