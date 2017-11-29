95104e958152c6901a19530ec7c3716044f1e48f
tnx6devine <tnx6devine@gmail.com>
2017-11-29 18:12:48 -0500

    set_up_username started

---
+++ signup_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require_relative "signup.rb"
+class TestSignup < Minitest::Test
+
+	def test_assert_1_is_1
+		assert_equal(1, 1)
+	end
+
+	def test_assert_that_usernames_are_strings
+		username = trixie
+		assert_equal(String, set_up_username(username))
+	end
+
+end
